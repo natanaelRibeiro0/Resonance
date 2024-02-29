7cac312992985179509bdb0451759a2ea8e06130
Natanael Ribeiro <natanaelworkstation@gmail.com>
2024-02-29 14:56:56 -0300

    Upload images to project (in public/site img) ; Started elaboration the pages passages from genders selectors; Add 3 more pages src/pages/movie, src/pages/searchPage, src/pages/trending

---
+++ src/app/searchPage/page.tsx
@@ -0,0 +1,10 @@
+
+export default function SearchPage(){
+  return(
+    <>
+      <h1>Text</h1>
+    </>
+  )
+}
+
+// https://api.themoviedb.org/3/discover/movie?api_key=[MY_KEY]&language=en-US&page=1&primary_release_year=2019&with_genres=28,12,80
