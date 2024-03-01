6766c67349604df73a949a5a2ae2a566a0b66020
Natanael Ribeiro <natanaelworkstation@gmail.com>
2024-02-29 23:05:32 -0300

    Edit de home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,69 +1,186 @@
 "use client"
 import Image from "next/image"
 import Link from "next/link"
-import { usePathname } from "next/navigation"
-
-// export const metadata: Metadata = {
-//   title: "Home",
-//   description: "Resonance homepage",
-// } 
-
 
 export default function Home(){
-
-  const pathname = usePathname()
-
   return (
     <>
-      <Image 
-      src = "/resonance-title.svg"
-      alt = "Resonance-Title"
-      width = "220"
-      height = "220"
-      priority
-      />
-
-      <hr />
-
       <div className="devChoicesContainer">
-        <div className="imgConatiner">
-        <Image 
-          src = "/black.png"
-          alt = "Resonance-Title"
-          width = "220"
-          height = "220"
-          />
-          Dev Choices 
+        <div className="imgConatiner grid place-items-center">
+        <Link 
+          href="">
+            <Image 
+              src = "/site img/DEV-CHOICES.png"
+              alt = "Dev-Choices"
+              width = {1000}
+              height = {0}
+            />
+        </Link>
         </div>
       </div>
 
-      <hr />
+      <h2 className="py-10 px-44 font-light text-4xl">Search your movie by genders</h2>
 
-      <h2> Genders </h2>
-    
-      <hr/>
-      
-      <div className="genderChoicesContainer flex gap-5 justify-center">
-        <Link href={{pathname: "/searchPage", query: { gender: "action"} }} className="genderCard bg-red-200 w-64 h-96">
-          {/* Background img === imagem do card */}
-          <h2>Action</h2>
+      <div className="genderChoicesContainer flex justify-center flex-wrap place-items-center">
+        <Link href={{pathname: "/searchPage", query: { gender: "Action"} }}>
+          <Image className = "hover:-translate-y-2 ease-in duration-150"
+            src={"/site img/genders/ACTION.png"}
+            alt="Action-Banner"
+            width={300}
+            height={300}
+          />
         </Link>
-      
-        <Link  href={{pathname: "/searchPage", query: "comedy"}} className="genderCard bg-red-300 w-64 h-96">
-          {/* Background img === imagem do card */}
-          <h2>Comedy</h2>
+
+        <Link href={{pathname: "/searchPage", query: { gender: "Adventure"} }}>
+          <Image className = "hover:-translate-y-2 ease-in duration-150"
+            src={"/site img/genders/ADVENTURE.png"}
+            alt="Adventure-Banner"
+            width={300}
+            height={300}
+          />
         </Link>
+
+        <Link href={{pathname: "/searchPage", query: { gender: "Animation"} }}>
+          <Image className = "hover:-translate-y-2 ease-in duration-150"
+            src={"/site img/genders/ANIMATION.png"}
+            alt="Animation-Banner"
+            width={300}
+            height={300}
+          />
+        </Link>
+
+        <Link href={{pathname: "/searchPage", query: { gender: "Comedy"} }}>
+          <Image className = "hover:-translate-y-2 ease-in duration-150"
+            src={"/site img/genders/COMEDY.png"}
+            alt="Comedy-Banner"
+            width={300}
+            height={300}
+          />
+        </Link>
+
+        <Link href={{pathname: "/searchPage", query: { gender: "Crime"} }}>
+          <Image className = "hover:-translate-y-2 ease-in duration-150"
+            src={"/site img/genders/CRIME.png"}
+            alt="Crime-Banner"
+            width={300}
+            height={300}
+          />
+        </Link>
+
+        <Link href={{pathname: "/searchPage", query: { gender: "Documentary"} }}>
+          <Image className = "hover:-translate-y-2 ease-in duration-150"
+            src={"/site img/genders/DOCUMENTARY.png"}
+            alt="Documentation-Banner"
+            width={300}
+            height={300}
+          />
+        </Link>
+
+        <Link href={{pathname: "/searchPage", query: { gender: "Drama"} }}>
+          <Image className = "hover:-translate-y-2 ease-in duration-150"
+            src={"/site img/genders/DRAMA.png"}
+            alt="Drama-Banner"
+            width={300}
+            height={300}
+          />
+        </Link>
+
+        <Link href={{pathname: "/searchPage", query: { gender: "Family"} }}>
+          <Image className = "hover:-translate-y-2 ease-in duration-150"
+            src={"/site img/genders/FAMILY.png"}
+            alt="Family-Banner"
+            width={300}
+            height={300}
+          />
+        </Link>
+
+        <Link href={{pathname: "/searchPage", query: { gender: "History"} }}>
+          <Image className = "hover:-translate-y-2 ease-in duration-150"
+            src={"/site img/genders/HISTORY.png"}
+            alt="History-Banner"
+            width={300}
+            height={300}
+          />
+        </Link>
+
+        <Link href={{pathname: "/searchPage", query: { gender: "Horror"} }}>
+          <Image className = "hover:-translate-y-2 ease-in duration-150"
+            src={"/site img/genders/HORROR.png"}
+            alt="Horror-Banner"
+            width={300}
+            height={300}
+          />
+        </Link>
+
+        <Link href={{pathname: "/searchPage", query: { gender: "Mistery"} }}>
+          <Image className = "hover:-translate-y-2 ease-in duration-150"
+            src={"/site img/genders/MISTERY.png"}
+            alt="Mistery-Banner"
+            width={300}
+            height={300}
+          />
+        </Link>
+
+        <Link href={{pathname: "/searchPage", query: { gender: "Music"} }}>
+          <Image className = "hover:-translate-y-2 ease-in duration-150"
+            src={"/site img/genders/MUSIC.png"}
+            alt="Music-Banner"
+            width={300}
+            height={300}
+          />
+        </Link>
+
       
-        <Link href={{pathname: "/searchPage", query: "drama"}} className="genderCard bg-red-400 w-64 h-96">
-          {/* Background img === imagem do card */}
-          <h2>Drama</h2>
+        <Link href={{pathname: "/searchPage", query: { gender: "Romance"} }}>
+          <Image className = "hover:-translate-y-2 ease-in duration-150"
+            src={"/site img/genders/ROMANCE.png"}
+            alt="Romance-Banner"
+            width={300}
+            height={300}
+          />
+        </Link>
+
+        <Link href={{pathname: "/searchPage", query: { gender: "Science Fiction"} }}>
+          <Image className = "hover:-translate-y-2 ease-in duration-150"
+            src={"/site img/genders/SCIENCE FICTION.png"}
+            alt="Science Fiction-Banner"
+            width={300}
+            height={300}
+          />
+        </Link>
+
+        <Link href={{pathname: "/searchPage", query: { gender: "Thriller"} }}>
+          <Image className = "hover:-translate-y-2 ease-in duration-150"
+            src={"/site img/genders/THRILLER.png"}
+            alt="Thriller-Banner"
+            width={300}
+            height={300}
+          />
+        </Link>
+
+        <Link href={{pathname: "/searchPage", query: { gender: "War"} }}>
+          <Image className = "hover:-translate-y-2 ease-in duration-150"
+            src={"/site img/genders/WAR.png"}
+            alt="War-Banner"
+            width={300}
+            height={300}
+          />
+        </Link>
+
+        <Link href={{pathname: "/searchPage", query: { gender: "Western"} }}>
+          <Image className = "hover:-translate-y-2 ease-in duration-150"
+            src={"/site img/genders/WESTERN.png"}
+            alt="Western-Banner"
+            width={300}
+            height={300}
+          />
         </Link>
       </div>
 
 
       <hr />
 
-      <h2> Trending </h2>
+      <h2 className="text-center"> Trending </h2>
 
       <hr />
 
@@ -73,7 +190,7 @@ export default function Home(){
           <h2>Trending Movie name</h2>
         </Link>
         
-        <Link href="/movie" className="genderCard bg-blue-200 w-52 h-64">
+        <Link href="/movie" className="genderCard bg-blue-300 w-52 h-64">
           {/* Background img === imagem do card */}
           <h2>Trending Movie name</h2>
         </Link>
--- src/app/searchPage/page.tsx
+++ src/app/searchPage/page.tsx
@@ -1,8 +1,100 @@
+"use client"
+import { useSearchParams } from "next/navigation"
+
+const dataGenders: Array<{id: Number; name: String}>  = [
+    {
+      "id": 28,
+      "name": "Action"
+    },
+    {
+      "id": 12,
+      "name": "Adventure"
+    },
+    {
+      "id": 16,
+      "name": "Animation"
+    },
+    {
+      "id": 35,
+      "name": "Comedy"
+    },
+    {
+      "id": 80,
+      "name": "Crime"
+    },
+    {
+      "id": 99,
+      "name": "Documentary"
+    },
+    {
+      "id": 18,
+      "name": "Drama"
+    },
+    {
+      "id": 10751,
+      "name": "Family"
+    },
+    {
+      "id": 14,
+      "name": "Fantasy"
+    },
+    {
+      "id": 36,
+      "name": "History"
+    },
+    {
+      "id": 27,
+      "name": "Horror"
+    },
+    {
+      "id": 10402,
+      "name": "Music"
+    },
+    {
+      "id": 9648,
+      "name": "Mystery"
+    },
+    {
+      "id": 10749,
+      "name": "Romance"
+    },
+    {
+      "id": 878,
+      "name": "Science Fiction"
+    },
+    {
+      "id": 10770,
+      "name": "TV Movie"
+    },
+    {
+      "id": 53,
+      "name": "Thriller"
+    },
+    {
+      "id": 10752,
+      "name": "War"
+    },
+    {
+      "id": 37,
+      "name": "Western"
+    }
+  ]
+  
 
 export default function SearchPage(){
+  // Usado para pegar o parametro passado na pagina home
+  const searchParams = useSearchParams()
+  // Genero selecionado
+  const anchor: String = (searchParams.get('gender'))
+  console.log(anchor)
+  const id = dataGenders.find((element: any) => {
+    const x: any = element.name === anchor
+    return x.id
+  })
+
   return(
     <>
-      <h1>Text</h1>
+      <h1>{anchor}</h1>
     </>
   )
 }
