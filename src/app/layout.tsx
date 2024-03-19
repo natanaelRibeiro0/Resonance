ecd99b95218e77cfcf954473ee2d88a1e7b97c1a
Natanael Ribeiro <natanaelworkstation@gmail.com>
2024-03-18 22:26:02 -0300

    Add Nav and Footer components, and, edit componentes order in body element

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Nav from "../components/navBar"
+import Nav from "../components/NavBar"
 import "./globals.css";
-import SideBar from "@/components/sideBar";
+import SideBar from "@/components/SideBar";
+import Footer from '../components/Footer';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,10 +19,11 @@ export default function RootLayout({
   children: React.ReactNode;}>) {
   return (
     <html lang="en">
-      <body className={inter.className} >  
-        <SideBar/>
-        <Nav />
+      <body className={inter.className}>  
+        <Nav></Nav>
+        <SideBar></SideBar>
         {children}
+        <Footer></Footer>
         </body>
     </html>
   );
