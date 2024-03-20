b6ac16f54305306de3d00afe7fd01e713fa2ec25
Natanael Ribeiro <natanaelworkstation@gmail.com>
2024-03-19 21:38:10 -0300

    Change elements styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Nav from "../components/NavBar"
+import Nav from "../components/Layout/NavBar"
 import "./globals.css";
-import SideBar from "@/components/SideBar";
-import Footer from '../components/Footer';
+import SideBar from "@/components/Layout/SideBar";
+import Footer from '../components/Layout/Footer';
 
 const inter = Inter({ subsets: ["latin"] });
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,16 @@
-import Image from "next/image"
 import Link from "next/link"
-import GenderCarrousel from "@/components/GenderCarrousel"
-import Trendings from "../components/Trendings"
+import GenderCarrousel from "@/components/Home Page/GenderCarrousel"
+import Trendings from "../components/Home Page/Trendings"
+import DevChoices from "@/components/Home Page/Dev Choice"
 
 export default function Home(){
   return (
     <main className="flex flex-col items-center justify-center gap-20">
       
-        <Link  href="" 
-        className="
-          imgConatiner 
-          flex items-end text-start text-white text-2xl font-semibold
-          w-3/5 h-96 p-10
-          shadow-2xl shadow-slate-400 bg-gradient-to-r from-purple-500 to-orange-400 rounded-lg" >
-            DEV CHOICES
-        </Link>
-
+      <DevChoices/>
+      
       <GenderCarrousel/>
+      
       <Trendings/>
 
   </main>
