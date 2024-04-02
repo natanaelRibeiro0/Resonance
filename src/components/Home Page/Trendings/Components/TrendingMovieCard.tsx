7b2ba1a14ccb75e3f703347f879df48609966383
Natanael Ribeiro <natanaelworkstation@gmail.com>
2024-04-01 21:59:17 -0300

    Make the popular movies display bigger

--- src/components/Home Page/Trendings/Components/TrendingMovieCard.tsx
+++ src/components/Home Page/Trendings/Components/TrendingMovieCard.tsx
@@ -24,7 +24,7 @@ export default function TrendingMovieCard( { key, props }: Props){
         
         { showMovieDetails ? 
           <div className="
-            p-2 pb-[4.7rem]
+            p-2 h-full
             absolute flex flex-col gap-2
             text-white 
             bg-zinc-950 bg-opacity-0
@@ -33,13 +33,13 @@ export default function TrendingMovieCard( { key, props }: Props){
           >
           <p className="font-bold text-center line-clamp-1 transition ease-in-out hover:-translate-y-1 hover:scale-110"> {props.title}</p>
           <hr/>
-          <p className="font-tin line-clamp-6"> {props.overview}</p>
+          <p className="font-tin line-clamp-[9]"> {props.overview}</p>
           <p >({props.releaseDate}) </p>
         </div>  : null }
         
         <Image 
           src={`https://image.tmdb.org/t/p/w400/${props.posterPath}`} 
-          width={200} 
+          width={250} 
           height={200} 
           alt="movie image"
         />
