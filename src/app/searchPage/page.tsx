42d812f6fa61612bed1a2961cb03aca658ae1bb4
Natanael Ribeiro <natanaelworkstation@gmail.com>
2024-04-18 16:27:53 -0300

    Start to develop the searchPage page

--- src/app/searchPage/page.tsx
+++ src/app/searchPage/page.tsx
@@ -1,92 +1,71 @@
 "use client"
 
-const dataGenders: Array<{id: Number; name: String}>  = [
-    {
-      "id": 28,
-      "name": "Action"
-    },
-    {
-      "id": 12,
-      "name": "Adventure"
-    },
-    {
-      "id": 16,
-      "name": "Animation"
-    },
-    {
-      "id": 35,
-      "name": "Comedy"
-    },
-    {
-      "id": 80,
-      "name": "Crime"
-    },
-    {
-      "id": 99,
-      "name": "Documentary"
-    },
-    {
-      "id": 18,
-      "name": "Drama"
-    },
-    {
-      "id": 10751,
-      "name": "Family"
-    },
-    {
-      "id": 14,
-      "name": "Fantasy"
-    },
-    {
-      "id": 36,
-      "name": "History"
-    },
-    {
-      "id": 27,
-      "name": "Horror"
-    },
-    {
-      "id": 10402,
-      "name": "Music"
-    },
-    {
-      "id": 9648,
-      "name": "Mystery"
-    },
-    {
-      "id": 10749,
-      "name": "Romance"
-    },
-    {
-      "id": 878,
-      "name": "Science Fiction"
-    },
-    {
-      "id": 10770,
-      "name": "TV Movie"
-    },
-    {
-      "id": 53,
-      "name": "Thriller"
-    },
-    {
-      "id": 10752,
-      "name": "War"
-    },
-    {
-      "id": 37,
-      "name": "Western"
-    }
-  ]
-  
+import { useState, useEffect, Suspense } from "react"
+import { useSearchParams } from "next/navigation"
+import Image from "next/image"
+import LoadingScreen from "@/app/loading"
+import querryResults from "@/components/SearchPage/FetchComponent/fetch"
 
-export default function SearchPage(){
-
-
-  return(
-    <>
-      <h1>text</h1>
-    </>
-  )
+interface Movie {
+  adult: Boolean ,
+  backdrop_path: string,
+  genre_ids: Array<number>
+  id: number,
+  original_language: string,
+  original_title: string,
+  overview: string,
+  popularity: number,
+  poster_path: string,
+  release_date: string,
+  title: string,
+  video: Boolean,
+  vote_average: number,
+  vote_count: number 
 }
 
+export interface GenderObjectInterface {
+  id: number, 
+  name: string
+}
+
+export default function Teste(){
+  const searchParams = useSearchParams()
+  const querry: string | null = searchParams.get("gender")
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    if (!querry) return; 
+
+    const fetchMovies = async () => {
+      try {
+        const response = await querryResults(querry);
+        setMovies(response.results)
+      } catch (error) {
+        console.error("Erro ao buscar filmes:", error);
+      }
+    };
+
+    fetchMovies();
+  }, [querry, movies]); 
+  return(
+    <div className="
+      pl-48
+      grid grid-cols-4 justify-between gap-10 place-items-center
+    "
+    >
+        { 
+          movies === undefined ? <h1 className="text-9xl text-red-500 p-64"></h1>:  
+          movies.map((element: Movie) => {
+            return (
+              <div key={element.id} className="border-4 border-black rounded-lg
+              ">
+                <img 
+                  src={`https://image.tmdb.org/t/p/w400/${element.poster_path}`} 
+                  alt="movie poster"
+                  />
+              </div>
+              )
+          })}
+    </div>
+    )
+}
