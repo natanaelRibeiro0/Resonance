ca831923e23aa999778185fa08c6c10073198de9
Natanael Ribeiro <natanaelworkstation@gmail.com>
2024-05-21 23:10:43 -0300

    modify items

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,25 @@
+"use client"
 import GenderCarrousel from "@/components/Home Page/GenderCarrousel"
 import Trendings from "../components/Home Page/Trendings"
 import DevChoices from "@/components/Home Page/Dev Choice"
 
+
+// interface Account{
+//   name: string,
+//   comments: Array<Object>
+// }
+
+// const account: Account = {
+//   name: "Nata",
+//   comments: []
+// }
+// localStorage.setItem('resonance-account', JSON.stringify(account))
+
 export default function Home(){
   return (
-    <main className="flex flex-col items-center justify-center gap-20">
+    <main className="flex flex-col items-center justify-center gap-20">  
       <DevChoices/>
-      
       <GenderCarrousel/>
-      
       <Trendings/>
     </main>
   )
