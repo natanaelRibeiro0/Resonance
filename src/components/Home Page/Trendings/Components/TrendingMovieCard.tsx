549b9419d299c4fb12584c2535516a54324cf763
Natanael Ribeiro <natanaelworkstation@gmail.com>
2024-05-21 22:32:19 -0300

    modyfied: atualize Trendings  components

--- src/components/Home Page/Trendings/Components/TrendingMovieCard.tsx
+++ src/components/Home Page/Trendings/Components/TrendingMovieCard.tsx
@@ -3,21 +3,30 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
-import { Movie } from "..";
   
 interface Props {
   key: number,
-  props: Movie 
+  genre_ids: Array<number>
+  id: number,
+  original_title: string,
+  overview: string,
+  poster_path: string,
+  release_date: string,
+  title: string,
+  video: Boolean,
+  vote_average: number,
 }
 
-export default function TrendingMovieCard( { key, props }: Props){
+export default function TrendingMovieCard( { key, genre_ids, id, original_title, overview, poster_path, release_date, title, vote_average }: Props){
+  const params = JSON.stringify({genre_ids, id, original_title, overview, poster_path, release_date, title, vote_average})
+
   const [ showMovieDetails, setShowMovieDetails] = useState(false)
   return(
     <>
       <Link 
-        href={""} key={key}
+        href={{ pathname: "/MoviePage", query : { movieDetails: params }}} key={key}
         className=" 
-        border-4 border-black backdrop-blur-sm"  
+        w-[300px] h-[430px] border-4 border-black backdrop-blur-sm shadow-2xl shadow-black"  
         onMouseEnter={ () =>  setShowMovieDetails(true)}
         onMouseLeave={ () =>  setShowMovieDetails(false)}
         >
@@ -28,20 +37,20 @@ export default function TrendingMovieCard( { key, props }: Props){
             absolute flex flex-col gap-2
             text-white 
             bg-zinc-950 bg-opacity-0
+            z-10
             transition ease-in-out 
             hover:bg-opacity-80"
           >
-          <p className="font-bold text-center line-clamp-1 transition ease-in-out hover:-translate-y-1 hover:scale-110"> {props.title}</p>
+          <p className="font-bold text-center line-clamp-1 transition ease-in-out hover:-translate-y-1 hover:scale-110"> {title}</p>
           <hr/>
-          <p className="font-tin line-clamp-[9]"> {props.overview}</p>
-          <p >({props.releaseDate}) </p>
+          <p className="font-tin line-clamp-[9]"> {overview}</p>
+          <p >{release_date}</p>
         </div>  : null }
         
-        <Image 
-          src={`https://image.tmdb.org/t/p/w400/${props.posterPath}`} 
-          width={250} 
-          height={200} 
+        <img 
+          src={`https://image.tmdb.org/t/p/w300/${poster_path}`}  
           alt="movie image"
+          className="w-full h-auto object-cover rounded-lg"
         />
       </Link>
   </>
--- src/components/Home Page/Trendings/index.tsx
+++ src/components/Home Page/Trendings/index.tsx
@@ -1,68 +1,67 @@
+import { useEffect, useState } from "react";
 import TrendingMovieCard from "./Components/TrendingMovieCard";
-
-interface PromiseResponse { 
-  id: number,
-  title: string,
-  overview: string,
-  poster_path: string,
-  release_date: string,
-}
+import getTrendingMovies from "./getTrendingMovies";
 
 export interface Movie {
-id: number;
-title: string, 
-overview: string, 
-posterPath: string, 
-releaseDate: string
+  id: number;
+  title: string, 
+  overview: string, 
+  poster_path: string, 
+  genre_ids: Array<number>,
+  original_title: string,
+  release_date: string,
+  vote_average: number
 }
 
-export default async function Trendings(){
+export default function Trendings(){
 
-  const options = {
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMTc5OTIzY2NmMWYwOWI5NTUxMmMzODMxMTQ5ZDgzNiIsInN1YiI6IjY1ZDNmZGNmMGYzNjU1MDE2M2U3ZTIxZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Kond84J8FQwHaBNTkFA-1UHuK8ZSqMmHt_aqakAQsjE'
-    }
-  };
-  
-  // Fetch operation to return trending movies
-  const promise = await fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
-    .then(response => response.json())
-  
-  const data: Array<Movie> = promise.results.map((element: PromiseResponse) => {
-      const movie: Movie = {
-        id: element.id,
-        title: element.title,
-        overview: element.overview,
-        posterPath: element.poster_path,
-        releaseDate: element.release_date,
+  const [trendingMovies, setTrendingMovies] = useState([])
+  const [loadState, setLoadState] = useState(false)
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await getTrendingMovies()
+        setTrendingMovies(response)
+
+      } catch (error) {
+        console.error("Error finding the trending movies", error)
       }
-      return movie
-    })
+    }
+    fetchMovies()
+    setLoadState(true)
+  }, [trendingMovies, loadState]) 
   
-    // Will slice elements from lenght > 9
-    data.splice((9))
 
   return(
   <>
-    
-    <h1 className="
-        p-5 
-        shadow-md shadow-black
-        font-mono italic 
-        rounded-lg bg-gradient-to-r from-purple-500 to-orange-400"> Trending today everywhere </h1>
+    { loadState === true ? 
+        <>
+        <h1 className="
+          p-5 
+          shadow-md shadow-black
+          font-mono italic 
+          rounded-lg bg-gradient-to-r from-purple-500 to-orange-400"> Trending today everywhere </h1>
 
-    <div className="grid grid-cols-3 gap-10">
-      {data.map((content: Movie) => {
-        return(
-          <TrendingMovieCard
-          key={ content.id }
-          props={ content }
-          />
-        )
-      })}
-    </div>
-  </> 
+        <div className="grid grid-cols-3 gap-10">
+          {trendingMovies.map((content: Movie) => {
+            return(
+              <TrendingMovieCard
+              key={ content.id }
+              genre_ids={content.genre_ids}
+              id={content.id}
+              original_title={content.title}
+              overview={content.overview}
+              poster_path={content.poster_path}
+              title={content.title}
+              vote_average={content.vote_average}
+              />
+            )
+          })}
+        </div>
+      </>
+    : null}
+      </> 
   )
 }
 
