bcbc560b13df2c3918aa79d9eeb7da6b2e8af940
Natanael Ribeiro <natanaelworkstation@gmail.com>
2024-05-22 21:28:13 -0300

    modify: change function

--- src/components/MoviePage/Aside.tsx
+++ src/components/MoviePage/Aside.tsx
@@ -1,5 +1,5 @@
 "use client"
-import Link from "next/link"
+import Link from "next/link";
 import Image from "next/image";
 
 interface Section {
@@ -10,26 +10,8 @@ interface Section {
 }
 
 interface GendersIds { 
-  28: string,
-  12 : string,
-  16 : string,
-  35 : string,
-  80 : string,
-  99 : string,
-  18 : string,
-  10751 : string,
-  14 : string,
-  36 : string,
-  27 : string,
-  10402 : string,
-  9648 : string,
-  10749 : string,  
-  878 : string,  
-  53 : string,
-  10752 : string,
-  37: string,
+  [key: number]: string;
 }
-
 
 const gendersIds: GendersIds = { 
   28: "ACTION",
@@ -52,23 +34,28 @@ const gendersIds: GendersIds = {
   37: "WESTERN"
 }
 
-
-export default function Aside( {poster_path, title, genre_ids, vote_avarage }: Section){  
-  return(
-      <aside  className="p-5 pb-30 min-w-[25%] h-max flex flex-col items-start justify-start text-white bg-gradient-to-b from-gray-600 to-black rounded-xl shadow-lg shadow-black">
-          <img src={`https://image.tmdb.org/t/p/w300/${poster_path}`}  alt={`${title} poster`} className="w-full h-auto object-cover rounded-lg"/>
-          <div className="w-10/12 pt-5 flex items-baseline gap-2 flex-wrap italic text-sm font-mono font-extralight">
-            { genre_ids.map( (number: number) => {
-              return (<> <Link  className="p-1 bg-gradient-to-b from-purple-600 to-orange-600 rounded-full ease-out hover:text-black hover:from-purple-300 hover:to-orange-300" href={{ pathname: "/GendersPage", query: { gender : gendersIds[number] }}}> { gendersIds[number]} </Link> </>)
-            })}
-          </div> 
-          <div className="pt-3 flex flex-wrap justify-between items-baseline">
-            <h1 className="font-light font-sans text-[30px]"> {title} </h1>
-          </div>
-          <div className="flex items-baseline">
-            <h1 className="font-light font-sans text-[60px]"> { vote_avarage.toFixed() } </h1>
-            <span className="font-light font-sans text-[20px]">  / 10  ★ ★ ★ </span>
-          </div>
+export default function Aside({poster_path, title, genre_ids, vote_avarage }: Section){  
+  return (
+    <aside className="p-5 pb-30 min-w-[25%] h-max flex flex-col items-start justify-start text-white bg-gradient-to-b from-gray-600 to-black rounded-xl shadow-lg shadow-black">
+      <img src={`https://image.tmdb.org/t/p/w300/${poster_path}`} alt={`${title} poster`} className="w-full h-auto object-cover rounded-lg"/>
+      <div className="w-10/12 pt-5 flex items-baseline gap-2 flex-wrap italic text-sm font-mono font-extralight">
+        {genre_ids.map((genderId: number) => (
+          <Link 
+            key={genderId} 
+            className="p-1 bg-gradient-to-b from-purple-600 to-orange-600 rounded-full ease-out hover:text-black hover:from-purple-300 hover:to-orange-300" 
+            href={{ pathname: "/GendersPage", query: { gender: gendersIds[genderId] } }}
+          > 
+            {gendersIds[genderId]} 
+          </Link>
+        ))}
+      </div>
+      <div className="pt-3 flex flex-wrap justify-between items-baseline">
+        <h1 className="font-light font-sans text-[30px]"> {title} </h1>
+      </div>
+      <div className="flex items-baseline">
+        <h1 className="font-light font-sans text-[60px]"> {vote_avarage.toFixed()} </h1>
+        <span className="font-light font-sans text-[20px]">  / 10  ★ ★ ★ </span>
+      </div>
     </aside>
-  )
+  );
 }
